19608d15a11b10879ac4e2c132dbf10a3274dcca
marinapetrenyuk <marina@ninesquares.studio>
2025-04-23 14:56:14 +0300

    formula styles

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const heroImage = document.querySelector("#hero .col:nth-child(2)"); // або вкажи конкретніше: .my-class, #my-id, і т.д.
-    if (heroImage) {
+    const heroSection = document.getElementById("#hero");
+    const heroImage = heroSection.querySelector(".col:nth-child(2)");
+    if (heroSection && heroImage) {
         heroImage.classList.add("image-blur");
+    }
+
+    const benefitsSection = document.getElementById("#benefits");
+    if(benefitsSection) {
+        let benefitsOffsetTop = benefitsSection.querySelector('.sqs-col-12').offsetTop;
+        console.log(benefitsOffsetTop);
     }
 
     window.onload = function() {
