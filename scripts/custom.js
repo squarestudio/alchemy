58a9e1170e8ce017e5335954a089171ab111ea02
marinapetrenyuk <marina@ninesquares.studio>
2025-04-23 14:49:26 +0300

    formula styles

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const heroImage = document.querySelector("#alchemy-hero .col:nth-child(2)"); // або вкажи конкретніше: .my-class, #my-id, і т.д.
     if (heroImage) {
-        heroImage.classList.add("animate-image");
+        heroImage.classList.add("image-blur");
     }
 
     window.onload = function() {
