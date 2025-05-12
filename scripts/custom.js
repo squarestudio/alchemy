de9d1bf97b068f9442502497a51583834c97d8c3
marinapetrenyuk <marina@ninesquares.studio>
2025-05-12 10:14:08 +0300

    manifesto styles

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,159 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const heroSection = document.getElementById("hero");
-    if (heroSection && heroSection.querySelector(".col:nth-child(2)")) {
-        heroSection.querySelector(".col:nth-child(2)").classList.add("image-blur");
-    }
 
-    const benefitsSection = document.getElementById("benefits");
-    let benefitsOffsetTop;
-    if (benefitsSection) {
-        let element = benefitsSection.querySelector('.sqs-col-12');
-        benefitsOffsetTop = element.getBoundingClientRect().top + window.scrollY - (window.innerHeight/1.6);
-    }
-
-    const sanctuarySection = document.getElementById("your-sweat-sanctuary");
-    let sanctuaryOffsetTop;
-    if (sanctuarySection) {
-        let element = sanctuarySection.querySelector('.sqs-col-12 > .row:nth-child(1)');
-        sanctuaryOffsetTop = element.getBoundingClientRect().top + window.scrollY;
-    }
-
-    const a37ClassesSection = document.getElementById("a37-classes");
-    let a37ClassesOffsetTop;
-    if (a37ClassesSection) {
-        a37ClassesOffsetTop = a37ClassesSection.getBoundingClientRect().top + window.scrollY + (window.innerHeight/2.5);
-    }
-
-    const classesSection = document.getElementById("classes");
-    let classesOffsetTop;
-    if(classesSection) {
-        classesOffsetTop = classesSection.getBoundingClientRect().top + window.scrollY;
-
-        const cols = classesSection.querySelectorAll('.Index-page-content > .sqs-layout > .row:nth-child(1) .col');
-        const items = classesSection.querySelectorAll('.Index-page-content > .sqs-layout > .row:nth-child(2) > .sqs-col-12 > .row.item');
-
-        cols.forEach((col, index) => {
-            col.addEventListener('click', () => {
-                cols.forEach(c => c.classList.remove('active'));
-                items.forEach(item => item.classList.remove('active'));
-
-                // Add 'active' class to the clicked col and the corresponding item
-                col.classList.add('active');
-                if (items[index]) {
-                    items[index].classList.add('active');
-                }
-            });
-        });
-        cols[0].click();
-    }
-
-    const blogSection = document.getElementById("blog");
-    if(blogSection) {
-        let blogLength = blogSection.querySelectorAll('.blog-item:not(.more-is-coming)').length;
-        let blogSwiper;
-        let blogHeight;
-        if(blogLength > 2) {
-            blogSwiper = new Swiper('#blog .swiper', {
-                slidesPerView: "auto",
-                centeredSlides: true,
-                spaceBetween: 24,
-                initialSlide: 1
-            });
-
-            blogHeight = blogSection.querySelector('.swiper-wrapper').offsetHeight;
-            console.log(blogHeight);
-
-            blogSwiper.on('slideChange', () => {
-                const realSlideCount = blogSwiper.slides.length;
-                if (blogSwiper.activeIndex === 0 || blogSwiper.activeIndex === realSlideCount - 1) {
-                    blogSwiper.slideTo(blogSwiper.previousIndex);
-                }
-            });
-        }else{
-            blogSection.classList.add('blogItems'+blogLength);
-
-            if(window.innerWidth <= 1024) {
-                blogSwiper = new Swiper('#blog .swiper', {
-                    slidesPerView: 1,
-                    pagination: {
-                        el: '.swiper-pagination',
-                    },
-                });
-
-                setTimeout(function(){
-                    blogHeight = blogSection.querySelector('.swiper-wrapper').offsetHeight;
-                    blogSection.querySelector('.swiper-wrapper').style.height = blogHeight+'px';
-                },500)
-            }else{
-                blogHeight = blogSection.querySelector('.swiper-wrapper').offsetHeight;
-                blogSection.querySelector('.swiper-wrapper').style.height = blogHeight+'px';
-            }
-        }
-    }
-
-    const a37ManifestoSection = document.getElementById("a37-manifesto");
-    let a37ManifestoOffsetTop;
-    if (a37ManifestoSection) {
-        a37ManifestoOffsetTop = a37ManifestoSection.getBoundingClientRect().top + window.scrollY + (window.innerHeight/2.5);
-    }
-
-    const welcomeSection = document.getElementById("welcome");
-    let welcomeOffsetTop;
-    if (welcomeSection) {
-        let element = welcomeSection.querySelector('.sqs-col-12');
-        welcomeOffsetTop = element.getBoundingClientRect().top + window.scrollY
-    }
-    const welcomePricingSection = document.getElementById("welcome-pricing");
-    let welcomePricingOffsetTop;
-    if (welcomePricingSection) {
-        let element = welcomePricingSection.querySelector('.sqs-col-12');
-        welcomePricingOffsetTop = element.getBoundingClientRect().top + window.scrollY
-    }
-    const welcomeFaqSection = document.getElementById("welcome-faq");
-    let welcomeFaqOffsetTop;
-    if (welcomeFaqSection) {
-        let element = welcomeFaqSection.querySelector('.sqs-col-12');
-        welcomeFaqOffsetTop = element.getBoundingClientRect().top + window.scrollY
-    }
-
-    const pricingImagesSection = document.getElementById("pricing-images");
-    let pricingImagesOffsetTop;
-    if (pricingImagesSection) {
-        pricingImagesOffsetTop = pricingImagesSection.getBoundingClientRect().top + window.scrollY + (window.innerHeight/2.5);
-    }
-
-    window.onload = function() {
-        document.querySelector('footer.Footer input[type="checkbox"]').checked = true;
-    }
-    window.addEventListener('scroll', () => {
-        const scrollPosition = window.scrollY || window.pageYOffset;
-        if(scrollPosition >= benefitsOffsetTop) {
-            document.body.classList.add('benefits-blur');
-        }
-        if(scrollPosition >= sanctuaryOffsetTop) {
-            document.body.classList.add('sanctuary-blur');
-        }
-        if(scrollPosition >= a37ClassesOffsetTop) {
-            document.body.classList.add('a37-blur');
-        }
-        if(scrollPosition >= classesOffsetTop) {
-            document.body.classList.add('classes-blur');
-        }
-        if(scrollPosition >= a37ManifestoOffsetTop) {
-            document.body.classList.add('manifesto-blur');
-        }
-        if(scrollPosition >= welcomeOffsetTop || scrollPosition >= welcomePricingOffsetTop || scrollPosition >= welcomeFaqOffsetTop) {
-            document.body.classList.add('welcome-blur');
-        }
-
-        if(scrollPosition >= pricingImagesOffsetTop) {
-            document.body.classList.add('pricing-images-blur');
-        }
-
-    });
-
-    /* HEADER GRADIENT */
-
+    /* ------------ HEADER GRADIENT ------------ */
     const header = document.querySelector(".Mobile-bar.Mobile-bar--top");
     const mobileOverlay = document.querySelector(".Mobile-overlay-menu");
 
@@ -242,4 +89,194 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     observer.observe(document.body, { attributes: true, attributeFilter: ["class"] });
+
+
+    /* ------------ HERO ------------ */
+    const heroSection = document.getElementById("hero");
+    if (heroSection && heroSection.querySelector(".col:nth-child(2)")) {
+        heroSection.querySelector(".col:nth-child(2)").classList.add("image-blur");
+    }
+
+
+    /* ------------ BENEFITS ------------ */
+    const benefitsSection = document.getElementById("benefits");
+    let benefitsOffsetTop;
+    if (benefitsSection) {
+        let element = benefitsSection.querySelector('.sqs-col-12');
+        benefitsOffsetTop = element.getBoundingClientRect().top + window.scrollY - (window.innerHeight/1.6);
+    }
+
+
+    /* ------------ SANCTUARY ------------ */
+    const sanctuarySection = document.getElementById("your-sweat-sanctuary");
+    let sanctuaryOffsetTop;
+    if (sanctuarySection) {
+        let element = sanctuarySection.querySelector('.sqs-col-12 > .row:nth-child(1)');
+        sanctuaryOffsetTop = element.getBoundingClientRect().top + window.scrollY;
+    }
+
+
+    /* ------------ A37 CLASSES ------------ */
+    const a37ClassesSection = document.getElementById("a37-classes");
+    let a37ClassesOffsetTop;
+    if (a37ClassesSection) {
+        a37ClassesOffsetTop = a37ClassesSection.getBoundingClientRect().top + window.scrollY + (window.innerHeight/2.5);
+    }
+
+    const classesSection = document.getElementById("classes");
+    let classesOffsetTop;
+    if(classesSection) {
+        classesOffsetTop = classesSection.getBoundingClientRect().top + window.scrollY;
+
+        const cols = classesSection.querySelectorAll('.Index-page-content > .sqs-layout > .row:nth-child(1) .col');
+        const items = classesSection.querySelectorAll('.Index-page-content > .sqs-layout > .row:nth-child(2) > .sqs-col-12 > .row.item');
+
+        cols.forEach((col, index) => {
+            col.addEventListener('click', () => {
+                cols.forEach(c => c.classList.remove('active'));
+                items.forEach(item => item.classList.remove('active'));
+
+                // Add 'active' class to the clicked col and the corresponding item
+                col.classList.add('active');
+                if (items[index]) {
+                    items[index].classList.add('active');
+                }
+            });
+        });
+        cols[0].click();
+    }
+
+
+    /* ------------ BLOG ------------ */
+    const blogSection = document.getElementById("blog");
+    if(blogSection) {
+        let blogLength = blogSection.querySelectorAll('.blog-item:not(.more-is-coming)').length;
+        let blogSwiper;
+        let blogHeight;
+        if(blogLength > 2) {
+            blogSwiper = new Swiper('#blog .swiper', {
+                slidesPerView: "auto",
+                centeredSlides: true,
+                spaceBetween: 24,
+                initialSlide: 1
+            });
+
+            blogHeight = blogSection.querySelector('.swiper-wrapper').offsetHeight;
+            console.log(blogHeight);
+
+            blogSwiper.on('slideChange', () => {
+                const realSlideCount = blogSwiper.slides.length;
+                if (blogSwiper.activeIndex === 0 || blogSwiper.activeIndex === realSlideCount - 1) {
+                    blogSwiper.slideTo(blogSwiper.previousIndex);
+                }
+            });
+        }else{
+            blogSection.classList.add('blogItems'+blogLength);
+
+            if(window.innerWidth <= 1024) {
+                blogSwiper = new Swiper('#blog .swiper', {
+                    slidesPerView: 1,
+                    pagination: {
+                        el: '.swiper-pagination',
+                    },
+                });
+
+                setTimeout(function(){
+                    blogHeight = blogSection.querySelector('.swiper-wrapper').offsetHeight;
+                    blogSection.querySelector('.swiper-wrapper').style.height = blogHeight+'px';
+                },500)
+            }else{
+                blogHeight = blogSection.querySelector('.swiper-wrapper').offsetHeight;
+                blogSection.querySelector('.swiper-wrapper').style.height = blogHeight+'px';
+            }
+        }
+    }
+
+
+    /* ------------ A37 MANIFESTO ------------ */
+    const a37ManifestoSection = document.getElementById("a37-manifesto");
+    let a37ManifestoOffsetTop;
+    if (a37ManifestoSection) {
+        a37ManifestoOffsetTop = a37ManifestoSection.getBoundingClientRect().top + window.scrollY + (window.innerHeight/2.5);
+    }
+
+
+    /* ------------ WELCOME ------------ */
+    const welcomeSection = document.getElementById("welcome");
+    let welcomeOffsetTop;
+    if (welcomeSection) {
+        let element = welcomeSection.querySelector('.sqs-col-12');
+        welcomeOffsetTop = element.getBoundingClientRect().top + window.scrollY
+    }
+    const welcomePricingSection = document.getElementById("welcome-pricing");
+    let welcomePricingOffsetTop;
+    if (welcomePricingSection) {
+        let element = welcomePricingSection.querySelector('.sqs-col-12');
+        welcomePricingOffsetTop = element.getBoundingClientRect().top + window.scrollY
+    }
+    const welcomeFaqSection = document.getElementById("welcome-faq");
+    let welcomeFaqOffsetTop;
+    if (welcomeFaqSection) {
+        let element = welcomeFaqSection.querySelector('.sqs-col-12');
+        welcomeFaqOffsetTop = element.getBoundingClientRect().top + window.scrollY
+    }
+
+
+    /* ------------ PRICING ------------ */
+    const pricingImagesSection = document.getElementById("pricing-images");
+    let pricingImagesOffsetTop;
+    if (pricingImagesSection) {
+        pricingImagesOffsetTop = pricingImagesSection.getBoundingClientRect().top + window.scrollY + (window.innerHeight/2.5);
+    }
+
+    /* ------------ FAQ ------------ */
+    const faqItemsSection = document.getElementById("faq-items");
+    if (faqItemsSection) {
+        faqItemsSection.querySelectorAll('ul li').forEach(item => {
+            item.addEventListener('click', () => {
+                // Toggle 'open' class
+                item.classList.toggle('open');
+
+                // Optionally, close others if you want only one open at a time
+                document.querySelectorAll('#faq-items ul li').forEach(otherItem => {
+                    if (otherItem !== item) {
+                        otherItem.classList.remove('open');
+                    }
+                });
+            });
+        });
+    }
+
+
+
+    window.onload = function() {
+        document.querySelector('footer.Footer input[type="checkbox"]').checked = true;
+    }
+    window.addEventListener('scroll', () => {
+        const scrollPosition = window.scrollY || window.pageYOffset;
+        if(scrollPosition >= benefitsOffsetTop) {
+            document.body.classList.add('benefits-blur');
+        }
+        if(scrollPosition >= sanctuaryOffsetTop) {
+            document.body.classList.add('sanctuary-blur');
+        }
+        if(scrollPosition >= a37ClassesOffsetTop) {
+            document.body.classList.add('a37-blur');
+        }
+        if(scrollPosition >= classesOffsetTop) {
+            document.body.classList.add('classes-blur');
+        }
+        if(scrollPosition >= a37ManifestoOffsetTop) {
+            document.body.classList.add('manifesto-blur');
+        }
+        if(scrollPosition >= welcomeOffsetTop || scrollPosition >= welcomePricingOffsetTop || scrollPosition >= welcomeFaqOffsetTop) {
+            document.body.classList.add('welcome-blur');
+        }
+
+        if(scrollPosition >= pricingImagesOffsetTop) {
+            document.body.classList.add('pricing-images-blur');
+        }
+
+    });
+
 });
