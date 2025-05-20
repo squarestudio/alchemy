58b5f1683b71e9fc16f96c7eb44789922918a02c
marinapetrenyuk <marina@ninesquares.studio>
2025-05-20 12:37:58 +0300

    manifesto styles`

--- scripts/custom.js
+++ scripts/custom.js
@@ -603,8 +603,8 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if(combineSection) {
-            let combineTriggerPoint = combineSection.offsetTop + 200;
-            let combineSecondTriggerPoint = combineSection.offsetTop + 400;
+            let combineTriggerPoint = combineSection.offsetTop + 300;
+            let combineSecondTriggerPoint = combineSection.offsetTop + 500;
             let combineScrollRange = Math.ceil(combineSecondTriggerPoint - combineTriggerPoint);
 
             if (scrollPosition >= combineTriggerPoint) {
