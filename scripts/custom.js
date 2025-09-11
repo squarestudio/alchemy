280030e27a00884d24bdca5758d02519ab0e05da
Marina Ninesquares <marina@ninesquares.studio>
2025-09-11 14:25:07 +0300

    changes

--- scripts/custom.js
+++ scripts/custom.js
@@ -487,7 +487,9 @@ window.Squarespace.onInitialize(Y, function() {
             newsletterFirstNameInput.setAttribute('placeholder', 'Full Name');
 
             let newsletterLastNameInput = newForm.querySelector('fieldset div.last-name input');
+            console.log(newsletterLastNameInput.value);
             newsletterLastNameInput.value = '-';
+            console.log(newsletterLastNameInput.value);
 
             let newsletterButton = newForm.querySelector('button[type="submit"]');
             if (!newsletterButton.querySelector('svg')) {
